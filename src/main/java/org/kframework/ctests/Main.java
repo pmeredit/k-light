a0d8b1898e7e9320e6d6800aa042da9d23d93574
Patrick Meredith <pmeredit@gmail.com>
2018-05-15 23:01:16 -0400

    hacked together test scripts

---
+++ src/main/java/org/kframework/ctests/Main.java
@@ -0,0 +1,89 @@
+package org.kframework.ctests;
+
+import org.kframework.attributes.Source;
+import org.kframework.definition.Definition;
+import org.kframework.kore.OuterToKORE;
+import org.kframework.parser.concrete2kore.ParserUtils;
+import org.kframework.utils.FileUtil;
+import org.kframework.utils.GlobalOptions;
+import org.kframework.utils.Stopwatch;
+import org.kframework.utils.errorsystem.KExceptionManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    private static final String startPath = "src/test/k/";
+    private static ParserUtils defParser = new ParserUtils(FileUtil.testFileUtil()::resolveWorkingDirectory, new KExceptionManager(new GlobalOptions()));
+
+    public static void main(String[] args) {
+        int total = 0, warnings = 0, ok = 0, error = 0, others = 0;
+        Stopwatch sw = new Stopwatch(new GlobalOptions(true, GlobalOptions.Warnings.ALL, true));
+        KExceptionManager kem = new KExceptionManager(new GlobalOptions());
+
+        try {
+            Object[] results = Files.find(Paths.get(startPath), Integer.MAX_VALUE,
+                    (filePath, fileAttr) -> fileAttr.isRegularFile() && filePath.toString().endsWith(".k"))
+                    .parallel()
+                    .map(fp -> {
+                        long startTime = System.currentTimeMillis();
+						System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+						System.out.println(fp.toFile());
+                        String rez = process(fp.toFile());
+                        System.out.println(rez);
+						long totalTime = System.currentTimeMillis() - startTime;
+                        if (rez.equals("[Error]")) {
+                            System.out.println("[Error]   " + fp.toFile().getAbsolutePath() + "   (" + totalTime + " ms)");
+                            return "[Error]";
+                        } else if (rez.equals("[Warning]")) {
+                            System.out.println("[Warning] " + fp.toFile().getAbsolutePath() + "   (" + totalTime + " ms)");
+                            return "[Warning]";
+                        }
+                        System.out.println("[ok]      " + fp.toFile().getAbsolutePath() + " (" + totalTime + " ms)");
+                        return "[ok]";
+                    }).toArray();
+            for (Object obj : results) {
+                String str = (String) obj;
+                switch (str) {
+                    case "[ok]": ok++; break;
+                    case "[Error]": error++; break;
+                    case "[Warning]": warnings++; break;
+                    case "[?????]": others++; break;
+                }
+            }
+            System.out.println("Ok: " + ok);
+            System.out.println("Error: " + error);
+            System.out.println("Warning: " + warnings);
+            System.out.println("Others: " + others);
+            System.out.println("Total: " + (ok + error + warnings + others));
+            sw.printTotal("Time: ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String process(File f) {
+		String koreParserCWD = "/home/pmeredit/K/kore/src/main/haskell/language-kore";
+        //String koreParserCWD = "c:\\work\\kore\\src\\main\\haskell\\"; // hardcoded for each machine - for testing only
+        Definition baseK = defParser.loadDefinition("INPUT", "INPUT", FileUtil.load(f), new Source(f.toString()), new ArrayList<>());
+
+        String str = OuterToKORE.apply(baseK);
+        String termination = ".kore.info";
+		File fl = new File(f.getAbsolutePath() + termination);
+        FileUtil.save(fl, str);
+		System.out.println(fl.getAbsolutePath());
+        try {
+            RunProcess.ProcessOutput po = RunProcess.execute(new HashMap<>(), new File(koreParserCWD), "/usr/local/bin/stack", "exec", "--", "kore-parser", fl.getAbsolutePath());
+			System.out.println(new String(po.stderr));
+            return po.exitCode != 0 ? "[Error]" : "[ok]";
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            return "[Error]";
+        }
+    }
+}
